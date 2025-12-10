a1cb0d75af61cea540a8e639670768e01a41c5f8
Chuck Butkus <chuck@all-hands.dev>
2025-12-10 01:02:13 -0500

    Revert "Try one more thing"
    This reverts commit 0c7b4573c9eafd33c2f0b847d088dc59acde28cf.

--- enterprise/storage/stored_conversation_metadata.py
+++ enterprise/storage/stored_conversation_metadata.py
@@ -1,5 +1,22 @@
-from openhands.app_server.app_conversation.sql_app_conversation_info_service import (
-    StoredConversationMetadata as _StoredConversationMetadata,
-)
+def _get_stored_conversation_metadata():
+    from openhands.app_server.app_conversation.sql_app_conversation_info_service import (
+        StoredConversationMetadata as _StoredConversationMetadata,
+    )
 
-StoredConversationMetadata = _StoredConversationMetadata
+    return _StoredConversationMetadata
+
+
+# Lazy import to avoid circular dependency
+StoredConversationMetadata = None
+
+
+def __getattr__(name):
+    global StoredConversationMetadata
+    if name == 'StoredConversationMetadata':
+        if StoredConversationMetadata is None:
+            StoredConversationMetadata = _get_stored_conversation_metadata()
+        return StoredConversationMetadata
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
+__all__ = ['StoredConversationMetadata']
